c8014dcef4741dcb9c5bc006a73d56f22bc91be2
iizhukov <iizhukov@icloud.com>
2025-10-26 02:38:14 +0300

    ADD: shutdown endpoint

--- services/connections/api/v1/router.py
+++ services/connections/api/v1/router.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from api.v1.endpoints import status, connections, working, images, health
+from api.v1.endpoints import status, connections, working, images, health, admin
 
 api_router = APIRouter()
 
@@ -9,3 +9,4 @@ api_router.include_router(connections.router, tags=["connections"])
 api_router.include_router(working.router, tags=["working"])
 api_router.include_router(images.router, tags=["images"])
 api_router.include_router(health.router, tags=["health"])
+api_router.include_router(admin.router, prefix="/admin", tags=["admin"])
