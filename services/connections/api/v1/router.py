b47ca5ceed78392e3bf8cd7ca542f1a40d0f0bbd
iizhukov <iizhukov@icloud.com>
2025-10-27 01:38:27 +0300

    RM: admin

--- services/connections/api/v1/router.py
+++ services/connections/api/v1/router.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from api.v1.endpoints import status, connections, working, images, health, admin
+from api.v1.endpoints import status, connections, working, images, health
 
 api_router = APIRouter()
 
@@ -9,4 +9,3 @@ api_router.include_router(connections.router, tags=["connections"])
 api_router.include_router(working.router, tags=["working"])
 api_router.include_router(images.router, tags=["images"])
 api_router.include_router(health.router, tags=["health"])
-api_router.include_router(admin.router, prefix="/admin", tags=["admin"])
