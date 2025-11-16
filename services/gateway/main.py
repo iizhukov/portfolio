5df678c824e127203019a8481978084a5c54eafe
iizhukov <iizhukov@icloud.com>
2025-11-16 13:27:21 +0300

    FIX: client IP & gateway CORS

--- services/gateway/main.py
+++ services/gateway/main.py
@@ -36,15 +36,9 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost",
-        "http://localhost:80",
-        "http://localhost:3000",
-        "http://localhost:5173",
-        "http://localhost:8080",
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
-    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_methods=["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allow_headers=["*"],
 )
 
